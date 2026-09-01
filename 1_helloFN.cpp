477bf0ab717814229848e43a4b52e368411ba6d7
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-09-01 22:04:25 +0530

    Function calling another function.

--- 1_helloFN.cpp
+++ 1_helloFN.cpp
@@ -2,12 +2,22 @@
 #include <iostream>
 using namespace std;
 
+// Creating a function to print hello world.
 void sayhello() {
     cout << "hello world :) \n";
+}
+
+// An assistant function to call the sayhello function.
+void assistant() {
+    sayhello();
+    cout << "Work is done through Aassistant \n";
+
 }
 
 int main() {
 
     sayhello();
+    assistant();
+    
     return 0;
 }
