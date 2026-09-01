76073de0e8131d1c8572518f72c3a03b409c54b4
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-09-01 21:49:30 +0530

    Function

---
+++ 1_helloFN.cpp
@@ -0,0 +1,13 @@
+// WAP to print hello world form different function.
+#include <iostream>
+using namespace std;
+
+void sayhello() {
+    cout << "hello world :) \n";
+}
+
+int main() {
+
+    sayhello();
+    return 0;
+}
