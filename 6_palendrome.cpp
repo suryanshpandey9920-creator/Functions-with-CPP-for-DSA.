c3c42a75c3f2810b1b33a8575b9acf21d1bdc85d
Suryansh Pandey <suryanshpandey9920@gmail.com>
2026-09-06 17:40:37 +0530

    Palendrome Number in CPP, using functions With Storing digit in another variable.

--- 6_palendrome.cpp
+++ 6_palendrome.cpp
@@ -7,8 +7,8 @@ void palendrome(int n) {
     int digit ;
     int pd = 0; 
     while (n > 0) {
-        digit = n % 10;
-        pd = (10* pd) + digit   ;
+        digit = n % 10; // Takes out the remainder.
+        pd = (10* pd) + digit;  // It store number in 0th,1th,10th,100th,1000th.
         digit = digit ;
         n = n / 10;
     }
